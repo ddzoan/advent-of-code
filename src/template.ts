8c48e048952c2ccd76f70476bddadc639e3e96c5
Dan Dzoan <dandzoan@gmail.com>
2024-12-08 23:11:03 -0800

    update template, add logger that doesn't log for main run

--- src/template.ts
+++ src/template.ts
@@ -1,5 +1,21 @@
 import { importFile } from './utils/utils';
+import { Logger } from './utils/utils';
 
 const data = importFile(__filename).trim();
-// const data = ``
+const testAnswer = 0;
+const testData = ``
 
+const run = (data, showLogs) => {
+    const clog = new Logger(showLogs);
+    clog.log()
+
+
+}
+
+const testRunResult = run(testData, true);
+console.log(testRunResult);
+
+if(testRunResult === testAnswer) {
+    console.log('Test data answer correct! Trying with real input')
+    console.log(run(data, false))
+}
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,6 +6,18 @@ export const importFile = (filename: string) => {
     return fs.readFileSync(`./src${inputFile}`).toString();
 }
 
+export class Logger {
+    showLogs: boolean;
+    constructor(showLogs: boolean) {
+        this.showLogs = showLogs;
+    }
+    log(...args) {
+        if(this.showLogs) {
+            console.log(args);
+        }
+    }
+}
+
 export const dirOffsetWithDiagonal = [
     [0, 1],
     [0, -1],
@@ -23,3 +35,8 @@ export const dirOffset = [
     [0, -1],
     [-1, 0],
 ]
+
+export const isInBounds = (position, grid) => {
+    const [row, col] = position;
+    return row >= 0 && col >= 0 && row < grid.length && col < grid[0].length;
+}
